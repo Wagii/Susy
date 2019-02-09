bdae746428af5be68a317e71a00c9146c3664530
Aurelien Chabani <aurelien.chabani@gmail.com>
2019-02-09 19:12:29 +0100

    Commit update gitignore, add ECS, add slower

---
+++ Assets/Resources/Scripts/ECS/Slower.cs
@@ -0,0 +1,42 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(GameObjectEntity))]
+public class Slower : MonoBehaviour {
+
+}
+
+class EntitySlow : ComponentSystem {
+	struct Components {
+		public Rigidbody rb;
+		public Slower sl;
+	}
+	
+	protected override void OnUpdate() {
+		struct Components {
+			public Rigidbody rb;
+			public ThrowObject to;
+		}
+	}
+	
+	private bool TestSleep(float magnitude, float angularMagnitude) {
+		if (magnitude == 0 || angularMagnitude == 0) return false;
+		if (magnitude >= Physics.sleepThreshold) return false;
+		if (angularMagnitude >= Physics.sleepThreshold) return false;
+		return true;
+	}
+	
+	protected override void OnUpdate() {
+		foreach (Components item in GetEntities<Components>()) {
+			if (item.rb.IsSleeping() == true) continue;
+			if (item.rb.isKinematic == true) continue;
+			item.rb.velocity -= item.rb.velocity * Time.deltaTime/* * Manager.slowParameters.slowSpeed*/;
+			if (TestSleep(item.rb.velocity.magnitude, item.rb.angularVelocity.magnitude)) {
+				item.rb.velocity = Vector3.zero;
+				item.rb.angularVelocity = Vector3.zero;
+				item.rb.Sleep();
+			}
+		}
+	}
+}
