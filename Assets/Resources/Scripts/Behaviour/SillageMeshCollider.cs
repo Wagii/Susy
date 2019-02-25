1277b2578f0f14ac96636bf6ea2a5a70e22e532c
logan <logan@monmail.com>
2019-02-25 20:21:53 +0100

    Add many things
    Breakable Cubes
    Test Scene
    Sillage Collider
    Fix sillage script (need more fixing)
    Fix CameraRig prefab
    Add 2 sillage models

---
+++ Assets/Resources/Scripts/Behaviour/SillageMeshCollider.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(MeshCollider))]
+public class SillageMeshCollider : MonoBehaviour
+{
+	[SerializeField] private MeshCollider collider;
+	[SerializeField] private Transform cam;
+	[Header("Speed on X, Size on Y")] [SerializeField] private AnimationCurve sizeCurve;
+	[SerializeField] private float sizeGrowthRate = .1f;
+	[SerializeField] private float objectSpeedMultiplier = 1f;
+	[Space]
+	[Header("Value for the transfer 3Dmodel->Unity (import scale)")]
+	[SerializeField] private float importScale = 1;
+	private float actualSpeed = 0;
+
+	private void Awake()
+	{
+		this.collider.isTrigger = true;
+		this.collider.enabled = false;
+		if (sizeCurve.length == 0)
+			DestroyImmediate(this);
+	}
+
+	private void FixedUpdate()
+	{
+		if (Manager.player.velocity.magnitude < sizeCurve.keys[0].time)
+		{		
+			if (this.collider.enabled) 
+				this.collider.enabled = false;
+			this.collider.transform.localScale = Vector3.one;
+			return;
+		} else if (!this.collider.enabled)
+			this.collider.enabled = true;
+	        
+		// TODO : Fix speed Lerp to make the collider grow organically
+		this.actualSpeed = Mathf.Lerp(this.actualSpeed, Manager.player.velocity.magnitude, Time.deltaTime);
+		
+		this.collider.transform.localScale = Vector3.one * this.sizeCurve.Evaluate(this.actualSpeed) * importScale;
+		this.collider.transform.LookAt(this.transform.position + Manager.player.velocity, Vector3.up);
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		Rigidbody rb = other.GetComponent<Rigidbody>();
+		if (rb == null) return;
+		rb.velocity += ((this.transform.position - rb.position).normalized * Manager.player.velocity.magnitude * this.objectSpeedMultiplier) - Manager.player.velocity.normalized;
+	}
+}
---
+++ Assets/Resources/Scripts/Universal/StickAtPosition.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StickAtPosition : MonoBehaviour
+{
+	[SerializeField] private Transform target;
+
+	protected void FixedUpdate() {
+		if (target == null) {
+			Destroy(this);
+			return;
+		}
+		
+		this.transform.position = this.target.position;
+    }
+}
